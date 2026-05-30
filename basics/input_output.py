b748a4f8800570b10bece3a30cf0a1067bb0d9ed
JAMAL KHAN <jk6366741@gmail.com>
2026-05-30 14:32:06 +0400

    Update input_output.py

--- basics/input_output.py
+++ basics/input_output.py
@@ -1,1 +1,9 @@
+# input and Output for Receiving Input
 
+Name = input("What is Your Name:")
+Age = input ("What is Your Age:")
+
+
+# The Output will receive for the Input That have been Received
+print("Name:",Name)
+print("Age:"age)
