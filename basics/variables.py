a971cd41634f9bcb6d4b772321f99cebb3a607d8
JAMAL KHAN <jk6366741@gmail.com>
2026-05-30 14:24:21 +0400

    Update variables.py

--- basics/variables.py
+++ basics/variables.py
@@ -1,1 +1,29 @@
+#variables.py
 
+#strings
+name = "Jamal Khan"
+print ("Name:",name)
+
+#integers
+age = 26
+print("Age:",age)
+
+#float
+height = 5.11
+print("Height:",height)
+
+#bolean
+is_learning_python = true
+print("Learning:"is_learning_python)
+
+#multiple Variables
+city,country = "Ajman","UAE"
+print("City:",city)
+print("Country:"country)
+# Variable reassignment
+score = 55
+print("Initial Score:"score)
+
+#updated Score
+score = 75
+print("Updated Score:"score)
