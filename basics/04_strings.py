c240a3e2f046b2c725e94cc517e55193dd08cf1c
MuhammadJamal553 <jk6366741@gmail.com>
2026-05-31 22:02:16 +0400

    Add strings practice and fix variables file

---
+++ basics/04_strings.py
@@ -0,0 +1,8 @@
+# 04_strings.py
+name = "Muhammad Jamal Tariq Nasrullah"
+print("My Name is:", name)
+print("Lenght:", len(name))
+print("UpperCase:", name.upper())
+print("LowerCase:", name.lower())
+print("First Letter:", name[0])
+
