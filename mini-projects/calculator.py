c6e2afb95a4b328ca41de00294e5bbb7846e18d3
MuhammadJamal553 <jk6366741@gmail.com>
2026-06-04 00:54:00 +0400

    Updated calculator logic

--- mini-projects/calculator.py
+++ mini-projects/calculator.py
@@ -1,4 +1,11 @@
+# ===== Simple Calculator =====
+# A simple calculator that can perform basic arithmetic operations
+# like addition, subtraction, multiplication, and division.
+# ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== 
+
 print("## Simple Calculator ##")
+# A loop to keep the calculator running until the user decides to exit.
+# ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== 
 while True:
     print("Choose An Option.")
     print("1. Add.")
@@ -6,13 +13,17 @@ while True:
     print("3. Multiple.")
     print("4. Divide.")
     print("5. Exit.")
+    # Option Selection.
     choice = input("Enter Your Choice (1-5): ")
+    # Exits If Don't Want to use
     if choice == "5":
         print("Exiting Calculator. Bye ")
         break
+    # Try Block To Catch Errors
     try:
         num1 = float(input("Enter The First Value: "))
         num2 = float(input("Enter The Second Value: "))
+        # Options Execution
         if choice == "1":
             print("Result:", num1 + num2)
             break
@@ -27,7 +38,9 @@ while True:
                 print("Error: Cannot Divide By Zero.")
             print("Result: ", num1 / num2)
             break
+        # If Choice Is Invalid
         else:
-            print("Invalid Choice")
+            print("Invalid Choice Try Again")
+    # If Input Is Not A Valid Number
     except ValueError:
         print("Enter A Valid Number")
