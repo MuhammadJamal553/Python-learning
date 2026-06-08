f915ce27ec7eb6db059b2c5bc0fe58f87432ceaf
MuhammadJamal553 <jk6366741@gmail.com>
2026-06-08 04:05:35 +0400

    Adding Brute Force Simulator

--- mini-projects/password_cracker.py
+++ mini-projects/password_cracker.py
@@ -1,20 +1,20 @@
 import itertools
 import time
-
+# Heading
 print("=== Brute Force Password Simulator ===")
-
+# 1. Get user input target
 target = input("Enter a 3 Digit lower case password to check :").strip().lower()
-
+# 2. Define the allowed character set
 chars = "abcdefghijklmnopqrstuvwxyz"
 start_time = time.time()
 attempts = 0
 cracked = False
 print("/nSearching for Match....")
-
+# 3. Generate all possible 3-letter combinations
 for guess_tuple in itertools.product(chars, repeat=3):
     attempts += 1
     guess = "".join(guess_tuple)
-
+    # Check if the guess matches the target
     if guess == target:
         end_time = time.time()
         duration = end_time - start_time
