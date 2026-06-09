36d1dba339f83fc113ce7686078b8be83ea70636
MuhammadJamal553 <jk6366741@gmail.com>
2026-06-10 00:02:40 +0400

    Updated README.md

--- mini-projects/password_cracker.py
+++ mini-projects/password_cracker.py
@@ -3,22 +3,22 @@ import time
 # Heading
 print("=== Brute Force Password Simulator ===")
 # 1. Get user input target
-target = input("Enter a 3 Digit lower case password to check :").strip().lower()
+target = input("Enter a 4-Digit PIN to check (0000-9999) :").strip().lower()
 # 2. Define the allowed character set
-chars = "abcdefghijklmnopqrstuvwxyz"
+chars = "0123456789"
 start_time = time.time()
 attempts = 0
 cracked = False
 print("/nSearching for Match....")
 # 3. Generate all possible 3-letter combinations
-for guess_tuple in itertools.product(chars, repeat=3):
+for guess_tuple in itertools.product(chars, repeat=4):
     attempts += 1
     guess = "".join(guess_tuple)
     # Check if the guess matches the target
     if guess == target:
         end_time = time.time()
         duration = end_time - start_time
-        print("/nPassword Cracked Seccesfully")
+        print("/n🔓 PIN Cracked Succesfully")
         print(f"Attempts :{attempts}")
         print(f"Decrypted Text : {guess}")
         print(f"Duration : {duration:.4f} Second")
