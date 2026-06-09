121cef86cb3771867ccd1c48192c70a224c82b0b
MuhammadJamal553 <jk6366741@gmail.com>
2026-06-10 00:45:06 +0400

    Add Pandas cybersecurity log analyzer and mock firewall data

--- data-analytics/log_analyzer.py
+++ data-analytics/log_analyzer.py
@@ -1,20 +1,22 @@
 import pandas as pd
-
+#Heading
 print("PANDAS CyberSecurity log Analyzer")
-
+# Anylyzing Data Path
 df = pd.read_csv("data-analytics/mock_security_logs.csv")
-
+# Printing the Data
 print ("Raw Log Data")
 print (df)
 
+# For Finding the Failed Or Suspesious logins
 print("Failed Suspesious Activities (Failed Login)")
 
 failed_logins = df[df["Status"] == "Failed"]
 
 print(failed_logins)
-
+# Total Bytes That are Used
 total_bytes = df["Bytes_Transferred"].sum()
 print(f"/n📊 Total Network Banwidth : {total_bytes} Bytes")
 
+# Saving a Report As A CSV of Failed Login for Analyzing
 failed_logins.to_csv("data-analytics/incident_report.cs", index=False)
 print("💾 Security Report is Exported to data-analytics/incident_report.csv")
