48ba9bd55e70be704040a7f5b174475707e626dd
MuhammadJamal553 <jk6366741@gmail.com>
2026-06-11 00:40:25 +0400

    Updated Log_analyzer.py

--- data-analytics/incident_report.cs
+++
@@ -1,4 +0,0 @@
-Timestamp,Username,IP_Address,Status,Bytes_Transferred
-2026-06-10 08:12:00,unknown,45.33.22.11,Failed,0
-2026-06-10 08:16:00,unknown,45.33.22.11,Failed,0
-2026-06-10 08:30:00,unknown,45.33.22.11,Failed,0
--- data-analytics/log_analyzer.py
+++ data-analytics/log_analyzer.py
@@ -7,16 +7,19 @@ df = pd.read_csv("data-analytics/mock_security_logs.csv")
 print ("Raw Log Data")
 print (df)
 
-# For Finding the Failed Or Suspesious logins
-print("Failed Suspesious Activities (Failed Login)")
+# 2. Aggregation: Group and count attacks by country location
+print("Top Suspesious Login Attempts By Country")
+Country_counts = df[df["Status"] == "Failed"]["Country"].value_counts()
+print(Country_counts)
+# 3. Advanced Filtering: Multi-condition lookup (& operator)
+# Looking for high-risk profiles: Failed logins where MFA was completely skipped/not triggered
+print("\n🚨 HIGHES RISK THREATS (Failed Status AND No MFA Enabled):")
+high_risk_threats = df[(df["Status"] == "Failed") & (df["MFA_Triggered"] == "No")]
+print(high_risk_threats[["Timestamp", "Username", "IP_Address", "Country"]])
 
-failed_logins = df[df["Status"] == "Failed"]
+# 4. Critical Statistical Metric: Find out average bandwidth of successful logins
+avg_bytes_success = df[df["Status"] == "Success"]["Bytes_Transferred"].mean()
+print(f"\nThe Baseline Bytes Transfered On Successfull Login {avg_bytes_success}")
 
-print(failed_logins)
-# Total Bytes That are Used
-total_bytes = df["Bytes_Transferred"].sum()
-print(f"/n📊 Total Network Banwidth : {total_bytes} Bytes")
-
-# Saving a Report As A CSV of Failed Login for Analyzing
-failed_logins.to_csv("data-analytics/incident_report.cs", index=False)
-print("💾 Security Report is Exported to data-analytics/incident_report.csv")
+high_risk_threats.to_csv("data-analytics/incident_report.csv", index=False )
+print("The Incident Report Is Save to data-analytics/incident_report.csv")
